74a1d7f4ebfa7edff7528c5187b798dc539b951a
Daryl Wright <daryl@goldenpath.ca>
2023-05-30 00:36:04 -0300

    docs: initialized documentation site

---
+++ docs/next.config.mjs
@@ -0,0 +1,41 @@
+import mdxConfig from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeGist from "rehype-gist";
+
+const withMDX = mdxConfig({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [
+      remarkGfm,
+    ],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypePrettyCode, {
+        theme: `one-dark-pro`,
+        keepBackground: true,
+        onVisitHighlightedLine: (node) => {
+          // Each line node by default has `class="line"`.
+          node.properties.className.push('highlighted');
+        },
+      }],
+      [rehypeGist, {
+        classNames: `not-prose my-4`,
+      }],
+    ],
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: [`tsx`, `jsx`, `md`, `mdx`],
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  output: `export`,
+  trailingSlash: true,
+};
+
+export default withMDX(nextConfig);
